139ba39bcd9f1d49094a0d9cc63015edca924b79
Oliver Cripps <ollycripps@gmail.com>
2019-06-14 14:38:07 +0100

    Creatae outlets and actions

--- Tabitha/ViewController.swift
+++ Tabitha/ViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func loginTapped(_ sender: Any) {
+    }
+    
 }
 
